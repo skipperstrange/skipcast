18914312da13df048ef73dbc368a9c4651927b1b
SkipperStrang (Nana Yaw Duah) <skipperstrange@gmail.com>
2025-02-02 11:08:03 +0000

    Setup reset. migration issues fixed

--- database/migrations/2025_01_25_000001_create_genres_table.php
+++ database/migrations/2025_01_25_000001_create_genres_table.php
@@ -1,1 +1,24 @@
- 
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGenresTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('genres', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('genre', 40);
+            $table->string('slug')->unique();
+            $table->timestamps();
+            $table->softDeletes(); // Added soft delete
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('genres');
+    }
+} 
--- database/migrations/2025_01_25_000001_create_media_genres_table.php
+++ database/migrations/2025_01_25_000001_create_media_genres_table.php
@@ -1,1 +1,25 @@
- 
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateMediaGenresTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('media_genres', function(Blueprint $table)
+        {
+            $table->bigInteger('media_id')->unsigned()->index();
+            $table->integer('genre_id')->unsigned()->index();
+            $table->foreign('genre_id')->references('id')->on('genres')->onDelete('cascade');
+            $table->foreign('media_id')->references('id')->on('media')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes(); // Added soft delete
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('media_genres');
+    }
+} 
